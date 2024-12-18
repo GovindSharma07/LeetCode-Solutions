c37a4617e5fec9e6b084d66ddae5164636cf1a5e
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2024-12-18 16:40:00 +0530

    Time: 27 ms (11.77%) | Memory: 40.9 MB (5.13%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.kt
+++ 13-roman-to-integer/roman-to-integer.kt
@@ -1,24 +1,34 @@
 class Solution {
     fun romanToInt(s: String): Int {
-        var total:Int = 0
-        val map : Map<String,Int> = mapOf(
-            "I" to            1,
-"V" to             5,
-"X"     to         10,
-"L"         to    50,
-"C"           to  100,
-"D"           to 500,
-"M"             to 1000,
+        // 1. Create a Map for Roman Numeral Values
+        val translations = mapOf(
+            "I" to 1,
+            "V" to 5,
+            "X" to 10,
+            "L" to 50,
+            "C" to 100,
+            "D" to 500,
+            "M" to 1000
         )
 
-        for( i in 0..<s.length-1){
-            if(map.getOrDefault(s[i].toString(),0) < map.getOrDefault(s[i+1].toString(),0)) total -= map.getOrDefault(s[i].toString(),0)
-            else total += map.getOrDefault(s[i].toString(),0)
+        // 2. Preprocess the Input String (Subtractive Notation Handling)
+        var modifiedString = s 
+            .replace("IV", "IIII")
+            .replace("IX", "VIIII")
+            .replace("XL", "XXXX")
+            .replace("XC", "LXXXX")
+            .replace("CD", "CCCC")
+            .replace("CM", "DCCCC")
+
+        // 3. Initialize the Result Variable
+        var number = 0
+
+        // 4. Iterate Through the String and Sum Values
+        for (char in modifiedString) {
+            number += translations.getValue(char.toString()) // Get value from map and add to total
         }
 
-        total += map.getOrDefault(s.last().toString(),0)
-
-
-        return total
+        // 5. Return the Final Result
+        return number
     }
 }
