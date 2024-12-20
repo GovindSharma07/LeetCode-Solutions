253ce43e16e6e14b4e3e4e0e4bc9ad23365b548f
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2024-12-20 16:46:08 +0530

    Time: 167 ms (97.46%) | Memory: 38.7 MB (21.94%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.kt
+++ 141-linked-list-cycle/linked-list-cycle.kt
@@ -1,15 +1,13 @@
 
 class Solution {
     fun hasCycle(head: ListNode?): Boolean {
-
-     var tortoise: ListNode? = head
-        var hare: ListNode? = head
-        while (tortoise != null && hare != null) {
-            tortoise = tortoise?.next
-            hare = hare?.next?.next
-            if (tortoise == hare && tortoise != null) {
-                return true
-            }
+     if(head == null) return false
+        var slow = head
+        var fast = head
+        while(fast != null && fast.next != null){
+            slow = slow?.next
+            fast = fast?.next?.next
+            if(slow == fast) return true
         }
         return false
     }
