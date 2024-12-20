05cd955d6f8a4e36ad343effaa8cb419222dcaf8
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2024-12-20 16:44:01 +0530

    Time: 199 ms (37.64%) | Memory: 39.1 MB (12.47%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.kt
+++ 141-linked-list-cycle/linked-list-cycle.kt
@@ -1,33 +1,16 @@
-/**
- * Example:
- * var li = ListNode(5)
- * var v = li.`val`
- * Definition for singly-linked list.
- * class ListNode(var `val`: Int) {
- *     var next: ListNode? = null
- * }
- */
 
 class Solution {
     fun hasCycle(head: ListNode?): Boolean {
-        
 
-        when{
-            head == null -> return false
-            head.next == null -> return false
-            head.next.next == null -> return false
-        } 
-
-        var s : ListNode? = head
-        var f : ListNode? = head?.next?.next
-
-        while(s != f){
-            f = f?.next
-            if(f == null) return false
-            f = f?.next
-            if(f == null) return false
-            s = s?.next
+     var tortoise: ListNode? = head
+        var hare: ListNode? = head
+        while (tortoise != null && hare != null) {
+            tortoise = tortoise?.next
+            hare = hare?.next?.next
+            if (tortoise == hare && tortoise != null) {
+                return true
+            }
         }
-        return true
+        return false
     }
 }
