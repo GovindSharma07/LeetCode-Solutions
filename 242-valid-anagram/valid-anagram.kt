c2c41222188eec30bdbddd802b3c7120880dab2b
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2024-12-29 08:45:48 +0530

    Time: 5 ms (92.72%) | Memory: 38.1 MB (58.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.kt
+++ 242-valid-anagram/valid-anagram.kt
@@ -1,22 +1,22 @@
 class Solution {
-    fun isAnagram(s: String, t: String): Boolean {
-        if(s.length != t.length) return false
+fun isAnagram(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
 
-        var map : MutableMap<Char,Int> = mutableMapOf()
+    var tChar = t.toCharArray()
+    var cChar = s.toCharArray()
+    var countArr = IntArray(26)
 
-        for(i in s){
-            if(map[i] == null) map[i] = 1
-            else map[i] = map[i]!!.plus(1)
-        }
-
-        for(i in t){
-            if(map[i] == null) return false
-            else{
-                if(map[i] == 0) return false
-                else map[i] = map[i]!!.minus(1)
-            }
-        }
-
-        return true
+    for (i in cChar) {
+        countArr[i - 'a']++
     }
+
+    for (i in tChar) {
+        countArr[i - 'a']--
+    }
+
+    for (i in countArr) {
+        if (i > 0) return false
+    }
+    return true
+}
 }
