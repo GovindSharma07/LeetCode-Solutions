854f01f01652e069b4e9125fd695382fb8b777e7
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2024-12-29 09:12:43 +0530

    Time: 0 ms (100.00%) | Memory: 37.9 MB (52.23%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.kt
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.kt
@@ -2,10 +2,9 @@ class Solution {
         fun sortedArrayToBST(nums: IntArray, low: Int = 0, high: Int = nums.size - 1): TreeNode? {
         if (low > high) return null
 
-        val mid = (high + low) / 2
-        return TreeNode(nums[mid]).apply{
-            left = sortedArrayToBST(nums, low, mid - 1)
-            right = sortedArrayToBST(nums, mid + 1, high)
+        return TreeNode(nums[(high + low) / 2]).apply{
+            left = sortedArrayToBST(nums, low, (high + low) / 2 - 1)
+            right = sortedArrayToBST(nums, (high + low) / 2 + 1, high)
         }
     }
 }
