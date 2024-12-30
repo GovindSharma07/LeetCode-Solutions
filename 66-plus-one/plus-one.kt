906de0bba808c958c7106332895e3385a3246df2
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2024-12-30 08:33:06 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (76.71%) - LeetSync

--- 66-plus-one/plus-one.kt
+++ 66-plus-one/plus-one.kt
@@ -1,6 +1,7 @@
 class Solution {
     fun plusOne(digits: IntArray): IntArray {
-        for (i in digits.size-  1 downTo 0) {
+       val n = digits.size
+        for (i in n - 1 downTo 0) {
             if (digits[i] < 9) {
                 digits[i]++
                 return digits
@@ -9,6 +10,9 @@ class Solution {
             digits[i] = 0
         }
 
-        return intArrayOf(1,*digits)
+        val newNumber = IntArray(n + 1)
+        newNumber[0] = 1
+
+        return newNumber
     }
 }
