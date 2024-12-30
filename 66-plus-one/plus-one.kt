ea91ff402b1ec9db7bdf2b35755d1641ca6ad6de
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2024-12-30 08:29:09 +0530

    Time: 3 ms (60.29%) | Memory: 35.9 MB (64.59%) - LeetSync

---
+++ 66-plus-one/plus-one.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+        for(i in (digits.size -1) downTo 0){
+            if(digits[i] == 9) {
+                digits[i] = 0
+                if(i == 0) return intArrayOf(1,*digits)
+            }
+            else {
+                digits[i] += 1
+                return digits
+            }
+        }
+        return digits
+    }
+}
