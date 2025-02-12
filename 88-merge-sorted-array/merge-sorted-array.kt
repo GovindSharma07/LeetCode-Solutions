b85f76709b24bbe277c130e8e43f76a6383548e3
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2025-02-12 20:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 36.2 MB (69.99%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.kt
+++ 88-merge-sorted-array/merge-sorted-array.kt
@@ -1,29 +1,24 @@
 class Solution {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        if(n == 0){
-            return
-        }
-
-        var a = m -1
-        var b = n -1
-        var k = m + n -1
-
-        while(a >= 0 && b >= 0){
-            if(nums1[a] >= nums2[b]){
-                nums1[k] = nums1[a]
-                a--
-            }
-            else{
-                nums1[k] = nums2[b]
-                b--
+        if(n == 0) return
+        var i = m-1
+        var j = n -1
+        var k = m + n - 1
+        while(j>=0 && i >=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k] = nums1[i]
+                i--
+            }else{
+                nums1[k] = nums2[j]
+                j--
             }
             k--
         }
-
-        while(b >= 0){
-            nums1[k] = nums2[b]
-            b--
+        while(j>=0){
+            nums1[k] = nums2[j]
             k--
+            j--
         }
+
     }
 }
