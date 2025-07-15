06dab92cb8fcb8b3982ffae7a105317e6632ac3f
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2025-07-15 11:40:58 +0530

    Time: 2 ms (78.07%) | Memory: 41.6 MB (87.36%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.kt
+++ 118-pascals-triangle/pascals-triangle.kt
@@ -1,27 +1,15 @@
 class Solution {
     fun generate(numRows: Int): List<List<Int>> {
         val ans = mutableListOf<List<Int>>()
-        if(numRows == 1){
-            ans.add(listOf(1))
-            return ans
+        
+        for (i in 0 until numRows) {
+            val row = MutableList(i + 1) { 1 }
+            for (j in 1 until i) {
+                row[j] = ans[i - 1][j - 1] + ans[i - 1][j]
+            }
+            ans.add(row)
         }
-        if(numRows == 2){
-            ans.add(listOf(1))
-            ans.add(listOf(1,1))
-            return ans
-        }
-                    
-        ans.add(listOf(1))
-        ans.add(listOf(1,1))
-    
-        for(i in 3..numRows){
-            val l = mutableListOf<Int>()
-            l.add(1)
-            for(j in 1..i-2) l.add(ans[i-2][j-1] + ans[i-2][j])
-            l.add(1)
-            ans.add(l)
-        }
-
+        
         return ans
     }
 }
