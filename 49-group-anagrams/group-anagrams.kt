cc16cfac1a489b7d12f04fb37f229f467fd101dd
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2025-07-15 20:04:53 +0530

    Time: 21 ms (90.07%) | Memory: 54.2 MB (53.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.kt
+++ 49-group-anagrams/group-anagrams.kt
@@ -1,32 +1,19 @@
-class Solution {
-    fun groupAnagrams(strs: Array<String>): List<List<String>> {
-        val ans = mutableListOf<MutableList<String>>()
-
-        for (i in strs.indices) {
-            var added = false
-            for (group in ans) {
-                if (isAna(strs[i], group[0])) {
-                    group.add(strs[i])
-                    added = true
-                    break
+    class Solution {
+        fun groupAnagrams(strs: Array<String>): List<List<String>> {
+            val map = HashMap<String, ArrayList<String>>()
+           
+            for (s in strs) {
+                val array = s.toCharArray()
+                array.sort()
+                //Two character arrays with the same elements but 
+                //different underlying storage might not be equal when converted to strings.
+                val key = String(array)
+                if (!map.contains(key)) {
+                    map[key] = arrayListOf()
                 }
+                map[key]?.add(s)
+
             }
-            if (!added) {
-                ans.add(mutableListOf(strs[i]))
-            }
+            return map.values.toList()
         }
-
-        return ans
     }
-
-    fun isAna(str1: String, str2: String): Boolean {
-        if (str1.length != str2.length) return false
-
-        val count = IntArray(26)
-        for (i in str1.indices) {
-            count[str1[i] - 'a']++
-            count[str2[i] - 'a']--
-        }
-        return count.all { it == 0 }
-    }
-}
