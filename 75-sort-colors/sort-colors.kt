5889100ebf2b531a575f2c59737f62e4c0255562
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2025-07-15 14:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (32.11%) - LeetSync

--- 75-sort-colors/sort-colors.kt
+++ 75-sort-colors/sort-colors.kt
@@ -1,10 +1,29 @@
 class Solution {
-    fun sortColors(nums: IntArray): Unit {
-        val l = MutableList(3){0}
+    fun sortColors(arr: IntArray): Unit {
+
+        //Bucket Sort
         var a = 0
-        nums.forEach{
-            l[it]++
+        var b = 0
+        var c = 0
+        for(i in arr) {
+            if(i == 0) a++
+            else if(i == 1) b++
+            else c++
         }
-        for(i in 0..2) for(j in 0 until l[i]) nums[a++] = i
+
+        for(i in 0..arr.size - 1) {
+            if(a > 0) {
+                arr[i] = 0
+                a--
+            }
+            else if(b > 0) {
+                arr[i] = 1
+                b--
+            }
+            else if(c > 0)  {
+                arr[i] = 2
+                c--
+            }
+        }
     }
 }
