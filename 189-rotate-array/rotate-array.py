a4892675882e38e2340197334db8466f57526d0b
Govind Sharma <114924409+GovindSharma07@users.noreply.github.com>
2025-10-19 15:07:22 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (18.95%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -5,30 +5,7 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
-        i = 0
-        j = len(nums)-k-1
-        while(i<j):
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
-            i+=1
-            j-=1
-        
-        i = len(nums)-k
-        j = len(nums)-1
-        while(i<j):
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
-            i+=1
-            j-=1
-        
-        i=0
-        j=len(nums)-1
-        while(i<j):
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
-            i+=1
-            j-=1
+        k = k %len(nums)
+        n = len(nums)
+        nums[0:0] = nums[n-k:]
+        nums[n:] = []
